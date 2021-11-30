a97969fd7d7f25b1c59f0dcd642aa49850d8cea9
josh.gohlke <jocago@gmail.com>
2021-11-30 08:55:11 -0500

    getting ready for 2021_01

---
+++ AoC/Puzzles/2021/Day 1/2021_01.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+struct p2021_1: Puzzle {
+    var data = testInput.parseToIntArray()
+    var runPart = 0
+
+    func part1() -> Any {
+        return 0
+    }
+
+    func part2() -> Any {
+        return 0
+    }
+}
+
+
+fileprivate let testInput = Data(raw: """
+
+""")
---
+++ AoC/Puzzles/2021/Day 1/2021_01_Input.swift
@@ -0,0 +1,10 @@
+//
+//  2021_01_Input.swift
+//  AoC
+//
+//  Created by Joshua Gohlke on 11/30/21.
+//
+
+let input_2021_01 = Data(raw: """
+        
+""")
