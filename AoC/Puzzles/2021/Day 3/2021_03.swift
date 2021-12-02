bc4a27b1a9fcd4b3353b654011be6dfeec98d99e
jocago <jocago@gmail.com>
2021-12-02 09:31:12 -0500

    Prepare for day 3

---
+++ AoC/Puzzles/2021/Day 3/2021_03.swift
@@ -0,0 +1,21 @@
+
+import Foundation
+
+struct p2021_3: Puzzle {
+    var data = testInput.parseToIntArray()
+    var runPart = 0
+
+    func part1() -> Any {
+        return 0
+    }
+
+    func part2() -> Any {
+        return 0
+    }
+}
+
+
+fileprivate let testInput = Data(raw: """
+
+""")
+
---
+++ AoC/Puzzles/2021/Day 3/2021_03_Input.swift
@@ -0,0 +1,10 @@
+//
+//  2021_03_Input.swift
+//  AoC
+//
+//  Created by Joshua Gohlke on 12/2/21.
+//
+
+let input_2021_3 = Data(raw: """
+
+""")
