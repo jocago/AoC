3a3edbc2a2bb70d5a104cc06ddd0e5ca6ed5acc1
jocago <jocago@gmail.com>
2021-12-02 09:30:31 -0500

    Update template for 2021

--- AoC/Puzzles/yyyy_dd_template.swift
+++ AoC/Puzzles/yyyy_dd_template.swift
@@ -1,8 +1,9 @@
 
 import Foundation
 
-struct p2015_x: Puzzle {
+struct p2021_x: Puzzle {
     var data = testInput.parseToIntArray()
+    //var data = input_2021_d.parseToIntArray()
     var runPart = 0
 
     func part1() -> Any {
@@ -19,6 +20,6 @@ fileprivate let testInput = Data(raw: """
 
 """)
 
-let input_20yy_dd = Data(raw: """
+let input_2021_d = Data(raw: """
         
 """)
