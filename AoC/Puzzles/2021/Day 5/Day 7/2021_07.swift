2f3433b30759b7d7998b76b204e169731eb5316b
jocago <jocago@gmail.com>
2021-12-12 12:54:52 -0500

    Day 7. Why did pt 2 take me so long???

--- AoC/Puzzles/2021/Day 5/Day 7/2021_07.swift
+++ AoC/Puzzles/2021/Day 5/Day 7/2021_07.swift
@@ -2,26 +2,49 @@
 import Foundation
 
 struct p2021_7: Puzzle {
-    var data = testInput.parseToStringArray()
-    //var data = input_2021_7.parseToStringArray()
-    var runPart = 1
+    //var data = testInput.parseToIntArray(splitBy: ",")
+    var data = input_2021_7.parseToIntArray(splitBy: ",")
+    var runPart = 2
+    
+    enum CalcMethod {
+        case linear, progressive
+    }
+    
+    func fuel(crabbies: [Int], method: CalcMethod) -> Int {
+        let fuelSlots = (crabbies.min()!...crabbies.max()!).map { gotoCrabbie in
+            crabbies
+                .map { crabbie in
+                    let diff = abs(crabbie - gotoCrabbie)
+                    switch method {
+                    case .linear:
+                        return diff
+                    case .progressive:
+                        return (diff * (diff + 1)) / 2
+                    }
+                }
+                .reduce(0, +)
+        }
+        return fuelSlots.min()!
+    }
+    
+    
 
     func part1() -> Any {
-        return 0
+        return fuel(crabbies: data, method: .linear)
     }
 
     func part2() -> Any {
-        return 0
+        return fuel(crabbies: data, method: .progressive)
     }
 }
 
 
 /*
- Pt 1 test target: ?
- pt 2 test target: ?
+ Pt 1 test target: 37
+ pt 2 test target: 168
  */
 fileprivate let testInput = Data(raw: """
-
+16,1,2,0,4,2,7,1,2,14
 """)
 
 
