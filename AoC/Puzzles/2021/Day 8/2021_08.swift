0e0e5cd82349ae7dcc6b6165aaa886e9765ae948
jocago <jocago@gmail.com>
2021-12-12 13:02:27 -0500

    Ready for day 8

---
+++ AoC/Puzzles/2021/Day 8/2021_08.swift
@@ -0,0 +1,27 @@
+
+import Foundation
+
+struct p2021_8: Puzzle {
+    var data = testInput.parseToStringArray()
+    //var data = input_2021_8.parseToStringArray()
+    var runPart = 1
+
+    func part1() -> Any {
+        return 0
+    }
+
+    func part2() -> Any {
+        return 0
+    }
+}
+
+
+/*
+ Pt 1 test target: ?
+ pt 2 test target: ?
+ */
+fileprivate let testInput = Data(raw: """
+
+""")
+
+
