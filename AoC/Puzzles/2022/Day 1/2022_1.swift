6c7d2205eb35b5581fe05adc402eb209f79adf3d
jocago <jocago@gmail.com>
2022-12-01 00:24:09 -0500

    Day 1 or 2022

--- AoC/Puzzles/2022/Day 1/2022_1.swift
+++ AoC/Puzzles/2022/Day 1/2022_1.swift
@@ -2,16 +2,34 @@
 import Foundation
 
 struct p2022_1: Puzzle {
-    var data = testInput.parseToStringArray()
-    //var data = input_2021_x.parseToStringArray()
-    var runPart = 1
+    //var data = testInput.parseToBlockOfStringsArray()
+    var data = input_2022_1.parseToBlockOfStringsArray()
+    var runPart = 2
+    
+    func get_packs() -> [Int] {
+        var packs: [Int] = []
+        for l in data {
+            var x = 0
+            for e in l {
+                x +=  Int(e)!
+            }
+            packs.append(x)
+        }
+        return packs
+    }
 
     func part1() -> Any {
-        return 0
+        let packs = self.get_packs()
+        return packs.max()!
     }
 
     func part2() -> Any {
-        return 0
+        let packs = self.get_packs()
+        var cals = 0
+        for i in 0..<3 {
+            cals += packs.sorted().reversed()[i]
+        }
+        return cals
     }
 }
 
@@ -21,7 +39,20 @@ struct p2022_1: Puzzle {
  pt 2 test target: ?
  */
 fileprivate let testInput = Data(raw: """
+1000
+2000
+3000
 
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
 """)
 
 
